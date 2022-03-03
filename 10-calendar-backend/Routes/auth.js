2a0f6a38382a6c09d15ec60afa4a6f2101e538b4
Tomás Silva <tomas.silva@correo.ucu.edu.uy>
2022-03-03 12:51:14 -0300

    cambios

---
+++ 10-calendar-backend/Routes/auth.js
@@ -0,0 +1,30 @@
+// Rutas
+
+const { Router } = require("express");
+const { check } = require('express-validator');
+
+const router = Router();
+
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+// /api/auth/new
+router.post(
+    '/new',
+    [ // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio, debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    crearUsuario );
+
+router.post(
+    '/',
+    [ // middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio, debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    loginUsuario );
+
+router.get( '/renew', revalidarToken );
+
+module.exports = router;
---
+++ 10-calendar-backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+
+// Crear el servidor de express
+
+const app = express();
+
+// Directorio Público
+
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+
+app.use( '/api/auth', require('./Routes/auth') )
+
+// Escuchar peticiones
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
+} );
+
